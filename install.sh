a7ff305b3e7720eb341b99aa328a06cf590c35da
Viet Nguyen <viet@users.noreply.github.com>
2016-03-30 20:06:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@
 # on a system that you use for other purposes as it might delete important
 # files, truncate your databases, and otherwise do mean things to you.
 #
-# By default, this script will install the minecraftly code in the /m directory
+# By default, this script will install the minecraftly code in the /minecraftly directory
 # and all of its dependencies (including java, screen, libraries and database
 # servers) at the system level. The installed minecraftly will run on screen
 # and accessible via "screen -r" command. Configuring, changing settings, and
 # optimizing the server for performance boost is expected to be done outside 
 # the installed environment and is not something this script handles.
 #
-# Requirements: Google Cloud Platform server only (for now). Debian 8 or higher.
+# Requirements: This script works with Debian 8 or higher.
 #
 ###############################################################################
 
@@ -46,37 +46,25 @@ apt-get update -y
 apt-get install oracle-java8-set-default -y
 
 #Make some directories & download some preconfigured files
-mkdir /m
-mkdir /m/b1
-mkdir /m/b2
-wget -P /m/b1 https://storage.googleapis.com/minecraftly/test/BungeeCord.jar
-wget -P /m/b2 https://storage.googleapis.com/minecraftly/test/BungeeCord.jar
-mkdir /m/b1/plugins
-mkdir /m/b2/plugins
-wget -P /m/b1/plugins https://storage.googleapis.com/minecraftly/test/RedisBungee.jar
-wget -P /m/b2/plugins https://storage.googleapis.com/minecraftly/test/RedisBungee.jar
-wget -P /m/b1/plugins https://storage.googleapis.com/minecraftly/test/MinecraftlyBungee.jar
-wget -P /m/b2/plugins https://storage.googleapis.com/minecraftly/test/MinecraftlyBungee.jar
-mkdir /m/b1/plugins/MinecraftlyBungee/
-mkdir /m/b2/plugins/MinecraftlyBungee/
-wget -P /m/b1/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/config.yml
-wget -P /m/b2/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/config.yml
-wget -P /m/b1/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/motd.yml
-wget -P /m/b2/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/motd.yml
-mkdir /m/buildtools
-mkdir /m/s1
-mkdir /m/s2
-wget -P /m/s1 https://storage.googleapis.com/minecraftly/test/spigot.jar
-wget -P /m/s2 https://storage.googleapis.com/minecraftly/test/spigot.jar
-mkdir /m/s1/plugins
-mkdir /m/s2/plugins
-wget -P /m/s1/plugins https://storage.googleapis.com/minecraftly/test/Minecraftly.jar
-wget -P /m/s2/plugins https://storage.googleapis.com/minecraftly/test/Minecraftly.jar
-wget -P /m/s1/plugins https://storage.googleapis.com/minecraftly/test/ProtocolLib.jar
-wget -P /m/s2/plugins https://storage.googleapis.com/minecraftly/test/ProtocolLib.jar
-wget -P /m/s1/plugins https://storage.googleapis.com/minecraftly/test/Vault.jar
-wget -P /m/s2/plugins https://storage.googleapis.com/minecraftly/test/Vault.jar
-mkdir /m/worlds
+mkdir /minecraftly
+wget -P /minecraftly/bungeecord1 https://storage.googleapis.com/minecraftly/test/BungeeCord.jar
+wget -P /minecraftly/bungeecord2 https://storage.googleapis.com/minecraftly/test/BungeeCord.jar
+wget -P /minecraftly/bungeecord1/plugins https://storage.googleapis.com/minecraftly/test/RedisBungee.jar
+wget -P /minecraftly/bungeecord2/plugins https://storage.googleapis.com/minecraftly/test/RedisBungee.jar
+wget -P /minecraftly/bungeecord1/plugins https://storage.googleapis.com/minecraftly/test/MinecraftlyBungee.jar
+wget -P /minecraftly/bungeecord2/plugins https://storage.googleapis.com/minecraftly/test/MinecraftlyBungee.jar
+wget -P /minecraftly/bungeecord1/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/config.yml
+wget -P /minecraftly/bungeecord2/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/config.yml
+wget -P /minecraftly/bungeecord1/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/motd.yml
+wget -P /minecraftly/bungeecord2/plugins/MinecraftlyBungee https://storage.googleapis.com/minecraftly/test/motd.yml
+wget -P /minecraftly/spigot1 https://storage.googleapis.com/minecraftly/test/spigot.jar
+wget -P /minecraftly/spigot2 https://storage.googleapis.com/minecraftly/test/spigot.jar
+wget -P /minecraftly/pigots1/plugins https://storage.googleapis.com/minecraftly/test/Minecraftly.jar
+wget -P /minecraftly/spigot2/plugins https://storage.googleapis.com/minecraftly/test/Minecraftly.jar
+wget -P /minecraftly/spigot1/plugins https://storage.googleapis.com/minecraftly/test/ProtocolLib.jar
+wget -P /minecraftly/spigot2/plugins https://storage.googleapis.com/minecraftly/test/ProtocolLib.jar
+wget -P /minecraftly/spigot1/plugins https://storage.googleapis.com/minecraftly/test/Vault.jar
+wget -P /minecraftly/spigot2/plugins https://storage.googleapis.com/minecraftly/test/Vault.jar
 
 #Start servers for the first time to generate files
 cd /m/b1 && screen -dmS b1 java -jar BungeeCord.jar
@@ -85,40 +73,40 @@ screen -r b1 -X stuff 'end\n'
 cd /m/b2 && screen -dmS b2 java -jar BungeeCord.jar
 sleep 60
 screen -r b2 -X stuff 'end\n'
-cd /m/s1 && screen -dmS s1 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /m/worlds --port 25567
+cd /m/s1 && screen -dmS s1 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /mnt/worlds --port 25567
 sleep 60
 screen -r s1 -X stuff 'stop\n'
-cd /m/s2 && screen -dmS s2 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /m/worlds --port 25568
+cd /m/s2 && screen -dmS s2 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /mnt/worlds --port 25568
 sleep 60
 screen -r s2 -X stuff 'stop\n'
 
 #Configure some files
-sed -i "s/ host: 0.0.0.0:.*/ host: 0.0.0.0:25565/" /m/b1/config.yml
-sed -i "s/ host: 0.0.0.0:.*/ host: 0.0.0.0:25566/" /m/b2/config.yml
-sed -i "s/ip_forward: .*/ip_forward: true/" /m/b1/config.yml
-sed -i "s/ip_forward: .*/ip_forward: true/" /m/b2/config.yml
-sed -i "s/motd: .*/motd: 'BungeeCord 1'/" /m/b1/config.yml
-sed -i "s/motd: .*/motd: 'BungeeCord 2'/" /m/b2/config.yml
-sed -i "s/md_5:/minecraftly:/" /m/b1/config.yml
-sed -i "s/md_5:/minecraftly:/" /m/b2/config.yml
-sed -i "s/heartbeatPort: .*/heartbeatPort: 25567/" /m/s1/plugins/Minecraftly/config.yml
-sed -i "s/heartbeatPort: .*/heartbeatPort: 25568/" /m/s2/plugins/Minecraftly/config.yml
-sed -i "s/address: localhost:.*/address: localhost:25567/" /m/b1/config.yml
-sed -i "s/address: localhost:.*/address: localhost:25568/" /m/b2/config.yml
+sed -i "s/ host: 0.0.0.0:.*/ host: 0.0.0.0:25565/" /minecraftly/bungeecord1/config.yml
+sed -i "s/ host: 0.0.0.0:.*/ host: 0.0.0.0:25566/" /minecraftly/bungeecord2/config.yml
+sed -i "s/ip_forward: .*/ip_forward: true/" /minecraftly/bungeecord1/config.yml
+sed -i "s/ip_forward: .*/ip_forward: true/" /minecraftly/bungeecord2/config.yml
+sed -i "s/motd: .*/motd: 'BungeeCord 1'/" /minecraftly/bungeecord1/config.yml
+sed -i "s/motd: .*/motd: 'BungeeCord 2'/" /minecraftly/bungeecord2/config.yml
+sed -i "s/md_5:/minecraftly:/" /minecraftly/bungeecord1/config.yml
+sed -i "s/md_5:/minecraftly:/" /minecraftly/bungeecord2/config.yml
+sed -i "s/heartbeatPort: .*/heartbeatPort: 25567/" /minecraftly/spigot1/plugins/Minecraftly/config.yml
+sed -i "s/heartbeatPort: .*/heartbeatPort: 25568/" /minecraftly/spigot2/plugins/Minecraftly/config.yml
+sed -i "s/address: localhost:.*/address: localhost:25567/" /m/bungeecord1/config.yml
+sed -i "s/address: localhost:.*/address: localhost:25568/" /m/bungeecord2/config.yml
 HOSTNAME=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
-sed -i "s/server-id:.*/server-id: $HOSTNAME/" /m/b1/plugins/RedisBungee/config.yml
-sed -i "s/server-id:.*/server-id: $HOSTNAME/" /m/b2/plugins/RedisBungee/config.yml
-sed -i "s/level-name=.*/level-name=world1/" /m/s1/server.properties
-sed -i "s/level-name=.*/level-name=world2/" /m/s2/server.properties
-sed -i "s/online-mode=.*/online-mode=false/" /m/s1/server.properties
-sed -i "s/online-mode=.*/online-mode=false/" /m/s2/server.properties
-sed -i "s/bungeecord: .*/bungeecord: true/" /m/s1/spigot.yml
-sed -i "s/bungeecord: .*/bungeecord: true/" /m/s2/spigot.yml
-sed -i "s/connection-throttle: .*/connection-throttle: -1/" /m/s1/bukkit.yml
-sed -i "s/connection-throttle: .*/connection-throttle: -1/" /m/s2/bukkit.yml
+sed -i "s/server-id:.*/server-id: $HOSTNAME/" /minecraftly/bungeecord1/plugins/RedisBungee/config.yml
+sed -i "s/server-id:.*/server-id: $HOSTNAME/" /minecraftly/bungeecord2/plugins/RedisBungee/config.yml
+sed -i "s/level-name=.*/level-name=world1/" /minecraftly/spigot1/server.properties
+sed -i "s/level-name=.*/level-name=world2/" /minecraftly/spigot2/server.properties
+sed -i "s/online-mode=.*/online-mode=false/" /minecraftly/spigot1/server.properties
+sed -i "s/online-mode=.*/online-mode=false/" /minecraftly/spigot2/server.properties
+sed -i "s/bungeecord: .*/bungeecord: true/" /minecraftly/spigot1/spigot.yml
+sed -i "s/bungeecord: .*/bungeecord: true/" /minecraftly/spigot2/spigot.yml
+sed -i "s/connection-throttle: .*/connection-throttle: -1/" /minecraftly/spigot1/bukkit.yml
+sed -i "s/connection-throttle: .*/connection-throttle: -1/" /minecraftly/spigot2/bukkit.yml
 
 #Start servers to play
-cd /m/b1 && screen -dmS b1 java -jar BungeeCord.jar
-cd /m/b2 && screen -dmS b2 java -jar BungeeCord.jar
-cd /m/s1 && screen -dmS s1 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /m/worlds --port 25567
-cd /m/s2 && screen -dmS s2 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /m/worlds --port 25568
+cd /minecraftly/b1 && screen -dmS b1 java -jar BungeeCord.jar
+cd /minecraftly/b2 && screen -dmS b2 java -jar BungeeCord.jar
+cd /minecraftly/spigot1 && screen -dmS s1 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /mnt/worlds --port 25567
+cd /minecraftly/spigot2 && screen -dmS s2 java -Dcom.mojang.eula.agree=true -jar spigot.jar --world-dir /mnt/worlds --port 25568
