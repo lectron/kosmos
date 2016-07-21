5ee3a84c1ed99eb31dd779f814a48f7ec4f0b403
Cory Redmond <ace@ac3-servers.eu>
2016-07-21 16:11:42 +0100

    Add UUID operations.

--- Core/src/main/java/com/minecraftly/core/MinecraftlyUtil.java
+++ Core/src/main/java/com/minecraftly/core/MinecraftlyUtil.java
@@ -10,12 +10,28 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 /**
  * @author Cory Redmond <ace@ac3-servers.eu>
  */
 public class MinecraftlyUtil {
+
+	private static final Pattern UUID_DASH_PATTERN = Pattern.compile( "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})" );
+
+	public static UUID convertFromNoDashes(String uuidString) {
+		return UUID.fromString(UUID_DASH_PATTERN.matcher(uuidString).replaceAll("$1-$2-$3-$4-$5"));
+	}
+
+	public static String convertToNoDashes(UUID uuid) {
+		return convertToNoDashes(uuid.toString());
+	}
+
+	public static String convertToNoDashes(String uuidString) {
+		return uuidString.replace("-", "");
+	}
 
 	/**
 	 * Download text from a string url.
