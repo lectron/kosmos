02941e998869ee2274f053d379415e8ed57a1e15
MeltedLux <lux@melted.pw>
2016-10-01 01:16:04 +0100

    Update DebugListener.java

--- Bungee/src/main/java/com/minecraftly/bungee/listeners/DebugListener.java
+++ Bungee/src/main/java/com/minecraftly/bungee/listeners/DebugListener.java
@@ -33,7 +33,6 @@ public class DebugListener implements Listener {
 	private final MinecraftlyBungeeCore core;
 
 	Callback<Callable<Void>, PlayerInputPair> loadedCallback = param -> () -> {
-
 		try {
 			ScriptEngine engine = getScriptEngine( param.getPlayer() );
 			engine.eval( param.getInput() );
@@ -42,32 +41,26 @@ public class DebugListener implements Listener {
 			param.getPlayer().sendMessage( ChatColor.YELLOW + e.getClass().getName() );
 			param.getPlayer().sendMessage( ChatColor.YELLOW + e.getMessage() );
 		}
-
 		return null;
-
 	};
 
 	@EventHandler( priority = Byte.MAX_VALUE )
 	public void onPluginMessageReceived( PluginMessageEvent event ) {
-
-		if ( !(event.getSender() instanceof ProxiedPlayer) ) return;
+		if (!(event.getSender() instanceof ProxiedPlayer)) return;
 
 		final ProxiedPlayer player = ((ProxiedPlayer) event.getSender());
 		final String channel = event.getTag();
 		final byte[] message = event.getData();
 
 		try {
-
-			if ( !channel.equals( "NMCLY" ) || !player.hasPermission( "minecraftly.debug" ) ) return;
+			if ( !channel.equals("NMCLY") || !player.hasPermission("minecraftly.debug") ) return;
 
 			DataInputStream dis = new DataInputStream( new ByteArrayInputStream( message ) );
 			String input = dis.readUTF();
 
 			if ( input.equalsIgnoreCase( "URL" ) ) {
-
 				final String url = dis.readUTF();
 				core.getOriginObject().getProxy().getScheduler().runAsync( core.getOriginObject(), () -> {
-
 					try {
 						String downloadedInput = MinecraftlyUtil.downloadText( url );
 						loadedCallback.call( new PlayerInputPair( player, downloadedInput ) ).call();
@@ -76,11 +69,8 @@ public class DebugListener implements Listener {
 						player.sendMessage( ChatColor.YELLOW + e.getClass().getName() );
 						player.sendMessage( ChatColor.YELLOW + e.getMessage() );
 					}
-
 				} );
-
 			} else {
-
 				core.getOriginObject().getProxy().getScheduler().runAsync( core.getOriginObject(), () -> {
 					try {
 						loadedCallback.call( new PlayerInputPair( player, input ) ).call();
@@ -90,17 +80,14 @@ public class DebugListener implements Listener {
 						player.sendMessage( ChatColor.YELLOW + e.getMessage() );
 					}
 				} );
-
 			}
 
 		} catch ( IOException ex ) {
 			ex.printStackTrace();
 		}
-
 	}
 
 	private ScriptEngine getScriptEngine( ProxiedPlayer player ) {
-
 		core.getDebugger().put( "server", ProxyServer.getInstance() );
 
 		ScriptEngine engine = core.getDebugger().getEngine();
@@ -109,17 +96,12 @@ public class DebugListener implements Listener {
 			engine.eval( "function print( message ) { me.sendMessage( message ); }" );
 		} catch ( ScriptException ignored ) {
 		}
-
 		return engine;
-
 	}
 
 	@Data
 	private class PlayerInputPair {
-
 		private final ProxiedPlayer player;
 		private final String input;
-
 	}
-
 }
